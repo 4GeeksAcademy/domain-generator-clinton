22632a7e5100388ba9439f824c30bfcf087cf294
clintonixx <161770373+clintonixx@users.noreply.github.com>
2024-11-26 00:28:12 +0000

    domain generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,47 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let pronoun = ["the", "our", "his"];
+let adj = ["great", "big", "amazing"];
+let noun = ["jogger", "racoon", "dog", "merchant", "driver"];
+let extensions = [".com", ".net", ".us", ".io", ".org", ".es"];
+
+function generateDomains() {
+  let domains = [];
+
+  // Bucle anidado para combinar pronombres, adjetivos, sustantivos y extensiones
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let e = 0; e < extensions.length; e++) {
+          // Generamos el dominio básico concatenando pronoun, adj, noun y extension
+          let domain = pronoun[i] + adj[j] + noun[k] + extensions[e];
+          domains.push(domain); // Añadimos el dominio a la lista
+
+          // Si la extensión es ".es", intentamos hacer un "domain hack"
+          if (extensions[e] === ".es") {
+            let domainHack =
+              pronoun[i] +
+              adj[j] +
+              noun[k]
+                .replace("jogger", "jogg")
+                .replace("racoon", "rac")
+                .toLowerCase() +
+              ".es";
+            domains.push(domainHack); // Añadimos el "domain hack" a la lista
+          }
+        }
+      }
+    }
+  }
+
+  return domains;
+}
+
+// Llamamos a la función y mostramos los resultados
+let generatedDomains = generateDomains();
+
+// Imprimimos los dominios generados en consola
+generatedDomains.forEach(domain => {
+  console.log(domain);
+});
